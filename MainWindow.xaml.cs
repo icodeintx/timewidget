162c42f835afe973affc686c7df84945b2b6ae4b
AmaraCode LLC <amaracodellc@gmail.com>
2020-03-08 01:48:57 -0600

    initial code

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TimeWidgetWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ResizeMode = ResizeMode.CanMinimize;
+            StartUp();
+            this.Topmost = true;
+        }
+
+        
+        public void StartUp()
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(SetTime);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+
+        public void SetTime(object sender, EventArgs e)
+        {
+            
+            this.TimeLocal.Content = DateTime.Now.ToLongTimeString();
+            this.TimeUTC.Content = DateTime.UtcNow.ToLongTimeString();
+
+        }
+    }
+}
