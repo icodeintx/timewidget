2edbc47e6a5599af57b8ae607c36c783782306b6
Scott Durrett <icodeintx@gmail.com>
2022-12-17 02:58:10 -0600

    Update to .net 7 and modifies ui

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,51 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
-namespace TimeWidgetWPF
+namespace TimeWidgetWPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            this.ResizeMode = ResizeMode.CanMinimize;
-            StartUp();
-            this.Topmost = true;
-        }
+        InitializeComponent();
+        this.ResizeMode = ResizeMode.CanMinimize;
+        StartUp();
+        this.Topmost = true;
+    }
 
-        
-        public void StartUp()
-        {
-            DispatcherTimer dispatcherTimer = new DispatcherTimer();
-            dispatcherTimer.Tick += new EventHandler(SetTime);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-            dispatcherTimer.Start();
-        }
+    public void SetTime(object sender, EventArgs e)
+    {
+        this.TimeLocal.Content = DateTime.Now.ToString("HH:mm:ss");
+        this.TimeUTC.Content = DateTime.UtcNow.ToString("HH:mm:ss");
+    }
 
-
-        public void SetTime(object sender, EventArgs e)
-        {
-            
-            this.TimeLocal.Content = DateTime.Now.ToLongTimeString();
-            this.TimeUTC.Content = DateTime.UtcNow.ToLongTimeString();
-
-        }
+    public void StartUp()
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        dispatcherTimer.Tick += new EventHandler(SetTime);
+        dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+        dispatcherTimer.Start();
     }
 }
--- obj/Release/netcoreapp3.1/App.g.cs
+++ obj/Release/netcoreapp3.1/App.g.cs
@@ -44,7 +44,7 @@ namespace TimeWidgetWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace TimeWidgetWPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
         public static void Main() {
             TimeWidgetWPF.App app = new TimeWidgetWPF.App();
             app.InitializeComponent();
